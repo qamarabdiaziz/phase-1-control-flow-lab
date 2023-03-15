fa1399040e58a08c545e7eb673b9d3f1490aaba9
qamarabdiaziz <tushez98@gmail.com>
2023-03-15 07:24:18 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(_someValue){
+  if(_someValue<=400 ){
+    return "This one is on me!"
+  }
+  else if (_someValue>2000){
+    return "I will gladly take your thirty bucks."
 }
+  else if(_someValue>2500){
+    return "No can do."
+  }
+}
+function ternaryCheckCity(_someValue){
+  return (_someValue==="NYC")?  "Ok, sounds good" :"No go."
 
-function ternaryCheckCity(){
   // Write your code here!
 }
 
 function switchOnCharmFromTip(){
+  if (tip ="generous" )
+  return "Thank you so much"
+  else if(tip ="not so generous")
+  return "Thank you"
+  else return "Bye"
   // Write your code here!
 }
